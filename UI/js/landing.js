b157205483d70be4b0fdcc08760919f4b6931828
Geoffrey Willis <benwill2014@gmail.com>
2019-02-10 05:02:19 +0300

    On error from the API, prompt the user to signin again

--- UI/js/landing.js
+++ UI/js/landing.js
@@ -3,8 +3,13 @@ const message = document.getElementById("flash-message")
 const success = "green";
 const fail = "red";
 const url = 'http://localhost:5000/api/v1/red-flags';
-
-getAll_redflags()
+let response = {
+    title : null,
+    type : null,
+    text : null,
+    status : null,
+    createdOn : null
+}
 
 function getAll_redflags() {
     if (typeof(Storage) !== "undefined") {
@@ -31,19 +36,20 @@ function fetchAllRedFlags(token){
     })
     .then(function(data) {
         if (data["status"] === 200) {
-            displayData(data);
+            let results = storeResponse(data);
+            displayData(results);
         }
         else if (data["status"] === 404) {
             displayText(success, data['data'][0]);
         }
         else {
             throw new Error(data["error"]);
-        }
-        
-        
+        }        
     })
     .catch(function(error){
-        displayText(fail, error.message)
+        displayText(fail, error.message);
+        sessionStorage.clear();
+        window.location.replace("signin.html");
     });
 }
 
@@ -61,12 +67,12 @@ function displayError(error) {
 		document.body.insertBefore(p, incidents);
 }
 
-function createTable(title, type, text, status, date) {
+function createTable(data) {
     let table = `
     <table class="table-landing">
         <thead>
             <tr>
-                <th>${title}</th>
+                <th>${data.title}</th>
             </tr>
         </thead>
         <tbody>
@@ -74,12 +80,12 @@ function createTable(title, type, text, status, date) {
                 <td>
                     <div class="row" id="row-describe">
                         <div class="col-10 col-s-10">
-                            <p id="comment">${text}...<a id="more-link" href=#>More</a></p>
+                            <p id="comment">${data.text}...<button id="more-link">More</button></p>
                             <p class="status"id="status">
                                 <img src="images/redflag1.jpg" height="20px" width="20px">
                             </p>
-                            <p class="status" id="posted"><b>Posted on: ${date}</b></p>
-                            <p class="status"id="status"><b>Status:</b> ${status}</b></p>
+                            <p class="status" id="posted"><b>Posted on: ${data.createdOn}</b></p>
+                            <p class="status"id="status"><b>Status:</b> ${data.status}</b></p>
                         </div>
                         <div class="col-2 col-s-2" id="col-deco"></div>
                         <div class="row" id="row-footer">
@@ -98,15 +104,54 @@ function createTable(title, type, text, status, date) {
     return table;
 }
 
-function displayData(result) {
-    for(var i = 0; i < result.data.length; i++) {
-        let title = result.data[i].title;
-        let type =  result.data[i].type;
-        let text = result.data[i].comment;
-        let status =  result.data[i].status;
-        let createdOn = result.data[i].createdOn;
-        let less = text.slice(0, 250);
-        let table = createTable(title, type, text, status, createdOn);
-        document.getElementById("display-incidents").innerHTML += table;  
+function displayData(dataArray) {
+    for(let i = 0; i < dataArray.length; i++) {
+        data = dataArray[i].data();
+        let table = createTable(data);
+        document.getElementById("display-incidents").innerHTML += table; 
     }     
+}
+
+function storeResponse(data) {
+    let results = [];
+    for(let i = 0; i < data.data.length; i++) {
+        title = data.data[i].title;
+        type =  data.data[i].type;
+        text = data.data[i].comment;
+        status =  data.data[i].status;
+        createdOn = data.data[i].createdOn; 
+        
+        response = new ResponseObj(title, type, text, status, createdOn);
+        results.push(response)
+    }
+    return results;
+}
+
+class ResponseObj {
+    constructor(title, type, text, status, createdOn) {
+        this._title = title;
+        this._type = type;
+        this._text = text;
+        this._status = status;
+        this._createdOn = createdOn;
+    }
+    data() {
+        let obj = {
+            title : this._title,
+            type : this._type,
+            text : this._text,
+            status : this._status,
+            createdOn : this._createdOn
+        }
+        return obj;
+    }
+}
+
+function showMore(text) {
+    return text;
+}
+
+function showLess(text) {
+    let less = response.text.slice(0, 100);
+    return less;
 }
